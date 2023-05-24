751dea8c2967a73110340151e865808b53b44e9b
Daniel Gudmundsen <danielrgudmundsen@outlook.com>
2023-05-24 06:01:48 -0700

    Added clear search history feature & search optimization - Added a function to clear search history from local storage and the UI. - Connect the clear history function to a new 'Clear History' button. - Update the search history storage to keep track of city coordinates, to prevent unnecessary API calls. - Increase the search history limit from 6 to 20, but keep displaying only the last 6 searched cities. This is to prevent too many Google Geocoding API calls. - Add console logs for debugging purposes.

--- script.js
+++ script.js
@@ -6,18 +6,19 @@ const GOOGLE_API_KEY = 'AIzaSyAEp8dF9zMb8G4HQ44kNTAgRiCuXmMv2XU';
 let searchHistory = [];
 
 // Function to update search history
-const updateSearchHistory = (cityName) => {
+const updateSearchHistory = (cityName, stateCode, coordinates) => {
+  console.log('Updating search history...');
   // Check if city is already in search history and remove it if found
-  const existingIndex = searchHistory.indexOf(cityName);
+  const existingIndex = searchHistory.findIndex(city => city.name === cityName && city.state === stateCode);
   if (existingIndex > -1) {
     searchHistory.splice(existingIndex, 1);
   }
 
   // Add the city at the front of search history
-  searchHistory.unshift(cityName);
+  searchHistory.unshift({name: cityName, state: stateCode, coordinates: coordinates});
 
-  // Limit search history to the last 6 items
-  searchHistory = searchHistory.slice(0, 6);
+  // Limit search history to the last 20 items
+  searchHistory = searchHistory.slice(0, 20);
 
   // Save the updated search history in local storage
   localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
@@ -25,16 +26,18 @@ const updateSearchHistory = (cityName) => {
   // Update Search History in UI
   $('#search-history').empty(); // Clear old search history buttons
   // Create and append new buttons for each city in search history
-  for (let i = 0; i < searchHistory.length; i++) {
+  for (let i = 0; i < Math.min(searchHistory.length, 6); i++) {
     const button = $('<button>');
-    button.text(searchHistory[i]);
+    button.text(`${searchHistory[i].name}, ${searchHistory[i].state}`);
     button.click(() => {
       // Search again when search history button is clicked
-      const cityName = button.text();
-      searchCity(cityName);
+      const cityName = searchHistory[i].name;
+      const stateCode = searchHistory[i].state;
+      searchCity(cityName, stateCode, searchHistory[i].coordinates);
     });
     $('#search-history').append(button);
   }
+  console.log(`Search history updated: ${JSON.stringify(searchHistory)}`);
 };
 
 // Load search history from local storage when page is loaded
@@ -43,18 +46,33 @@ $(document).ready(() => {
   if (savedSearchHistory) {
     searchHistory = JSON.parse(savedSearchHistory);
     // Update the search history in the UI
-    for (let i = 0; i < searchHistory.length; i++) {
+    for (let i = 0; i < Math.min(searchHistory.length, 6); i++) {
       const button = $('<button>');
-      button.text(searchHistory[i]);
+      button.text(`${searchHistory[i].name}, ${searchHistory[i].state}`);
       button.click(() => {
         // Search again when search history button is clicked
-        const cityName = button.text();
-        searchCity(cityName);
+        const cityName = searchHistory[i].name;
+        const stateCode = searchHistory[i].state;
+        searchCity(cityName, stateCode, searchHistory[i].coordinates);
       });
       $('#search-history').append(button);
     }
   }
 });
+
+// Function to clear search history
+const clearSearchHistory = () => {
+  // Clear search history array
+  searchHistory = [];
+
+  // Clear local storage
+  localStorage.removeItem('searchHistory');
+
+  // Clear UI
+  $('#search-history').empty();
+
+  console.log('Search history cleared.');
+};
 
 // Function to convert temperature from Kelvin to Fahrenheit
 const kelvinToFahrenheit = (kelvin) => {
@@ -67,11 +85,23 @@ const getDateString = (timestamp) => {
   return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
 }
 
-// Fetch city coordinates from Google Geocoding API
+// Fetch city coordinates from Google Geocoding API or local storage
 const getCoordinates = async (cityName, stateCode) => {
+  console.log(`Getting coordinates for ${cityName}, ${stateCode}...`);
+  // Check local storage first
+  const existingCity = searchHistory.find(city => city.name === cityName && city.state === stateCode);
+  if (existingCity) {
+    console.log('Coordinates found in search history.');
+    return existingCity.coordinates;
+  }
+
+  console.log('Coordinates not found in search history. Calling Google Geocoding API...');
+
+  // Call Google Geocoding API if city is not in search history
   const data = await $.getJSON(`https://maps.googleapis.com/maps/api/geocode/json?address=${cityName},${stateCode}&components=country:US&key=${GOOGLE_API_KEY}`);
   if (data.results && data.results.length > 0) {
     const location = data.results[0].geometry.location;
+    console.log(`Coordinates received: ${JSON.stringify(location)}`);
     return {
       lat: location.lat,
       lon: location.lng
@@ -88,15 +118,18 @@ const getWeatherData = async (lat, lon) => {
 }
 
 // Search weather data for a given city
-const searchCity = async (cityName, stateCode) => {
-  // Get coordinates from Google Geocoding
-  const coordinates = await getCoordinates(cityName, stateCode);
+const searchCity = async (cityName, stateCode, coordinates) => {
+  console.log(`Searching weather for ${cityName}, ${stateCode}...`);
+  // Get coordinates from Google Geocoding or local storage
+  if (!coordinates) {
+    coordinates = await getCoordinates(cityName, stateCode);
+  }
 
   // Get the weather data using the coordinates
   const weatherData = await getWeatherData(coordinates.lat, coordinates.lon);
 
   // Update the search history
-  updateSearchHistory(cityName);
+  updateSearchHistory(cityName, stateCode, coordinates);
 
   // Display the weather data in the UI
   displayCurrentWeather(weatherData);
@@ -151,13 +184,16 @@ const displayForecast = (weatherData) => {
   }
 }
 
+// Set up event listener for the clear history button
+$('#clear-history-button').on('click', clearSearchHistory);
+
 // Set up event listener for the search form
 $('#search-form').on('submit', (event) => {
   event.preventDefault();
 
   // Get city and state from the form
-  const cityName = $('#city').val();
-  const stateCode = $('#state').val();
+  const cityName = $('#city-input').val();
+  const stateCode = $('#state-select').val();
 
   // Validate input
   if (!cityName.trim()) {
