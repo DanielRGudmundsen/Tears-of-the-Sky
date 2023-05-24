7d0d8e88778c1de741146a57e99d1ef32b65ee88
Daniel Gudmundsen <danielrgudmundsen@outlook.com>
2023-05-23 19:59:36 -0700

    Add Google Maps Geocoding API as Fallback for Coordinates -In the event submission listener for the search form, I have added a fallback mechanism to fetch the latitude and longitude of a city. If the call to the OpenWeather API fails to get the coordinates, it now uses the Google Maps Geocoding API as a backup. -If both attempts to fetch coordinates fail, the application logs an error message, alerts the user, and returns, avoiding any subsequent operations.

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
+// Define API keys for OpenWeatherMap and Google
 const API_KEY = '66e7f108454dedaaf041fe29273aa7ee';
+const GOOGLE_API_KEY = 'AIzaSyAEp8dF9zMb8G4HQ44kNTAgRiCuXmMv2XU'; 
+
+// Identify key elements in the DOM
 const searchForm = document.querySelector('#search-form');
 const currentWeather = document.querySelector('#current-weather');
 const forecast = document.querySelector('#forecast');
 
+// Function to get coordinates using OpenWeatherMap API
 const getCoordinates = async (cityName) => {
   const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
   const data = await response.json();
 
-  // Log the data to the console
   console.log(`Coordinates for ${cityName}:`, data);
 
   return data[0];
 };
 
+// Function to get coordinates using Google Maps API as a fallback
+const getCoordinatesFromGoogle = async (cityName, stateCode) => {
+  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${cityName},${stateCode}&key=${GOOGLE_API_KEY}`);
+  const data = await response.json();
+
+  console.log(`Coordinates for ${cityName}, ${stateCode} from Google:`, data);
+
+  if (data.results && data.results.length > 0) {
+    const location = data.results[0].geometry.location;
+    return {
+      lat: location.lat,
+      lon: location.lng
+    };
+  }
+
+  return null;
+};
+
+// Function to get weather data using OpenWeatherMap API
 const getWeatherData = async (lat, lon) => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
   const data = await response.json();
 
-  // Log the data to the console
   console.log(`Weather data for lat=${lat}, lon=${lon}:`, data);
 
   return data;
 };
 
+// Event listener for form submission
 searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
@@ -33,13 +56,22 @@ searchForm.addEventListener('submit', async (event) => {
   console.log(`Form submitted with city name: ${cityName} and state: ${stateCode}`);
     
   try {
-    const coordinates = await getCoordinates(cityName + ',' + stateCode);
+    // Try to get coordinates using OpenWeatherMap API
+    let coordinates = await getCoordinates(cityName + ',' + stateCode);
 
+    // If OpenWeatherMap API fails, fall back to Google Maps API
+    if (!coordinates) {
+      console.log('OpenWeather API failed. Falling back to Google Maps API.');
+      coordinates = await getCoordinatesFromGoogle(cityName, stateCode);
+    }
+
+    // If both attempts fail, log an error and return
     if (!coordinates) {
       console.error('No coordinates found for city:', cityName);
       return;
     }
 
+    // Get weather data using OpenWeatherMap API
     const weatherData = await getWeatherData(coordinates.lat, coordinates.lon);
       
     // Clear the previous data
@@ -53,6 +85,5 @@ searchForm.addEventListener('submit', async (event) => {
   }
 });
 
-// TODO: Find an alternative way to get citys longitude and latitude if OpenWeatherMap API is not working. I want to be able to find Oakhurst, CA and its coordinates. Maybe an additional api?
 
   
