280cd3b03d250dbb64914215126f199a9be46249
Daniel Gudmundsen <danielrgudmundsen@outlook.com>
2023-05-23 23:37:40 -0700

    Major Update: Implemented Search History, Updated UI to Display Weather Data, and Transitioned to jQuery for API and Event Handling - Updated jQuery from version 3.5.1 slim build to 3.6.0 full version to leverage ajax functionality. - Added a container for displaying the search history with a relevant header. This provides users with a historical overview of their past weather queries. - Minor refactoring and rewording of comments to improve readability and maintain consistency in the codebase. - Transitioned from JavaScript's fetch function to jQuery's $.getJSON for API requests. - Implemented a search history feature storing the last six queries in local storage and re-performing searches when corresponding buttons are clicked. - Added helper functions to convert temperature from Kelvin to Fahrenheit and Unix timestamp to a human-readable date string. - Updated UI display logic to show weather data and forecast - Replaced .addEventListener() with jQuery's .on() method for form submission.

--- script.js
+++ script.js
@@ -1,29 +1,81 @@
-// Define API keys for OpenWeatherMap and Google
+// API keys for OpenWeather and Google Geocoding
 const API_KEY = '66e7f108454dedaaf041fe29273aa7ee';
 const GOOGLE_API_KEY = 'AIzaSyAEp8dF9zMb8G4HQ44kNTAgRiCuXmMv2XU'; 
 
-// Identify key elements in the DOM
-const searchForm = document.querySelector('#search-form');
-const currentWeather = document.querySelector('#current-weather');
-const forecast = document.querySelector('#forecast');
+// Initialize search history
+let searchHistory = [];
 
-// Function to get coordinates using OpenWeatherMap API
+// Function to update search history
+const updateSearchHistory = (cityName) => {
+  // Check if city is already in search history and remove it if found
+  const existingIndex = searchHistory.indexOf(cityName);
+  if (existingIndex > -1) {
+    searchHistory.splice(existingIndex, 1);
+  }
+
+  // Add the city at the front of search history
+  searchHistory.unshift(cityName);
+
+  // Limit search history to the last 6 items
+  searchHistory = searchHistory.slice(0, 6);
+
+  // Save the updated search history in local storage
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+  // Update Search History in UI
+  $('#search-history').empty(); // Clear old search history buttons
+  // Create and append new buttons for each city in search history
+  for (let i = 0; i < searchHistory.length; i++) {
+    const button = $('<button>');
+    button.text(searchHistory[i]);
+    button.click(() => {
+      // Search again when search history button is clicked
+      const cityName = button.text();
+      searchCity(cityName);
+    });
+    $('#search-history').append(button);
+  }
+};
+
+// Load search history from local storage when page is loaded
+$(document).ready(() => {
+  const savedSearchHistory = localStorage.getItem('searchHistory');
+  if (savedSearchHistory) {
+    searchHistory = JSON.parse(savedSearchHistory);
+    // Update the search history in the UI
+    for (let i = 0; i < searchHistory.length; i++) {
+      const button = $('<button>');
+      button.text(searchHistory[i]);
+      button.click(() => {
+        // Search again when search history button is clicked
+        const cityName = button.text();
+        searchCity(cityName);
+      });
+      $('#search-history').append(button);
+    }
+  }
+});
+
+// Function to convert temperature from Kelvin to Fahrenheit
+const kelvinToFahrenheit = (kelvin) => {
+  return ((kelvin - 273.15) * 9/5 + 32).toFixed(2);
+}
+
+// Function to convert Unix timestamp to human-readable date string
+const getDateString = (timestamp) => {
+  const date = new Date(timestamp * 1000);
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+}
+
+// Fetch city coordinates from OpenWeatherMap Geocoding API
 const getCoordinates = async (cityName) => {
-  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
-  const data = await response.json();
-
-  console.log(`Coordinates for ${cityName}:`, data);
-
+  const data = await $.getJSON(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
   return data[0];
 };
 
-// Function to get coordinates using Google Maps API as a fallback
+// Fetch city coordinates from Google Geocoding API as a fallback
 const getCoordinatesFromGoogle = async (cityName, stateCode) => {
-  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${cityName},${stateCode}&key=${GOOGLE_API_KEY}`);
-  const data = await response.json();
-
-  console.log(`Coordinates for ${cityName}, ${stateCode} from Google:`, data);
-
+  const data = await $.getJSON(`https://maps.googleapis.com/maps/api/geocode/json?address=${cityName},${stateCode}&key=${GOOGLE_API_KEY}`);
   if (data.results && data.results.length > 0) {
     const location = data.results[0].geometry.location;
     return {
@@ -31,60 +83,92 @@ const getCoordinatesFromGoogle = async (cityName, stateCode) => {
       lon: location.lng
     };
   }
-
   return null;
 };
 
-// Function to get weather data using OpenWeatherMap API
+// Fetch weather forecast data from OpenWeatherMap API
 const getWeatherData = async (lat, lon) => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-  const data = await response.json();
+  return await $.getJSON(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+}
 
-  console.log(`Weather data for lat=${lat}, lon=${lon}:`, data);
+// Search weather data for a given city
+const searchCity = async (cityName, stateCode) => {
+  let coordinates;
+  try {
+    // Attempt to get coordinates from OpenWeatherMap
+    coordinates = await getCoordinates(cityName);
+  } catch (error) {
+    // If it fails, use Google Geocoding as a fallback
+    coordinates = await getCoordinatesFromGoogle(cityName, stateCode);
+  }
 
-  return data;
-};
+  // Get the weather data using the coordinates
+  const weatherData = await getWeatherData(coordinates.lat, coordinates.lon);
 
-// Event listener for form submission
-searchForm.addEventListener('submit', async (event) => {
+  // Update the search history
+  updateSearchHistory(cityName);
+
+  // Display the weather data in the UI
+  displayCurrentWeather(weatherData);
+  displayForecast(weatherData);
+}
+
+// Function to display the current weather data in the UI
+const displayCurrentWeather = (weatherData) => {
+  // Clear old data
+  $('#current-weather').empty();
+  
+  // Convert temperatures from Kelvin to Fahrenheit
+  const temp = kelvinToFahrenheit(weatherData.current.temp);
+  const feels_like = kelvinToFahrenheit(weatherData.current.feels_like);
+
+  // Convert Unix timestamp to date string
+  const dateString = getDateString(weatherData.current.dt);
+
+  // Append new data
+  $('#current-weather').append(`
+    <h2>Current Weather</h2>
+    <p>Temperature: ${temp}°F</p>
+    <p>Feels Like: ${feels_like}°F</p>
+    <p>Humidity: ${weatherData.current.humidity}%</p>
+    <p>Date: ${dateString}</p>
+  `);
+}
+
+// Function to display the forecast data in the UI
+const displayForecast = (weatherData) => {
+  // Clear old data
+  $('#forecast').empty();
+
+  // Append new data
+  for (let i = 1; i <= 5; i++) {
+    // Convert temperatures from Kelvin to Fahrenheit
+    const temp = kelvinToFahrenheit(weatherData.daily[i].temp.day);
+    const feels_like = kelvinToFahrenheit(weatherData.daily[i].feels_like.day);
+
+    // Convert Unix timestamp to date string
+    const dateString = getDateString(weatherData.daily[i].dt);
+
+    $('#forecast').append(`
+      <div class="day">
+        <h3>Day ${i}</h3>
+        <p>Temperature: ${temp}°F</p>
+        <p>Feels Like: ${feels_like}°F</p>
+        <p>Humidity: ${weatherData.daily[i].humidity}%</p>
+        <p>Date: ${dateString}</p>
+      </div>
+    `);
+  }
+}
+
+// Set up event listener for the search form
+$('#search-form').on('submit', (event) => {
   event.preventDefault();
 
-  // Get city name and state code from the form
-  const cityName = event.target.elements['city-input'].value;
-  const stateCode = event.target.elements['state-select'].value;
+  // Get city and state from the form
+  const cityName = $('#city').val();
+  const stateCode = $('#state').val();
 
-  console.log(`Form submitted with city name: ${cityName} and state: ${stateCode}`);
-    
-  try {
-    // Try to get coordinates using OpenWeatherMap API
-    let coordinates = await getCoordinates(cityName + ',' + stateCode);
-
-    // If OpenWeatherMap API fails, fall back to Google Maps API
-    if (!coordinates) {
-      console.log('OpenWeather API failed. Falling back to Google Maps API.');
-      coordinates = await getCoordinatesFromGoogle(cityName, stateCode);
-    }
-
-    // If both attempts fail, log an error and return
-    if (!coordinates) {
-      console.error('No coordinates found for city:', cityName);
-      alert("Sorry, your city was not found. Please check your spelling and try again.");
-      return;
-    }
-
-    // Get weather data using OpenWeatherMap API
-    const weatherData = await getWeatherData(coordinates.lat, coordinates.lon);
-      
-    // Clear the previous data
-    currentWeather.textContent = '';
-    forecast.textContent = '';
-  
-    // TODO: Display the weather data in your HTML
-      
-  } catch (error) {
-    console.error('Error fetching weather data:', error);
-  }
+  // Search for the weather data
+  searchCity(cityName, stateCode);
 });
-
-
-  
